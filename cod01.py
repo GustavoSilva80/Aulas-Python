e51828edab51868530725142773b0675fe38b48b
Gustavo Rodrigues da Silva <91676784+GustavoSilva80@users.noreply.github.com>
2023-03-24 12:45:10 +0000

    Códigos passados na aula do dia 17\02\2023

---
+++ cod01.py
@@ -0,0 +1,11 @@
+semana=5
+semana=6
+semana="sexta"
+unidade="Vila Prudente"
+nota= 7.8
+nota="alface"
+print("Semana:",semana)
+print("Unidade:",unidade)
+print("Nota:",nota)
+print("\nSemana:",semana,"\nNota:",nota,"\nUnidade:",unidade)
+#a ultima variavel é a que ia aparecer no shell
---
+++ cod02.py
@@ -0,0 +1,5 @@
+#boolean true or false
+luz_acesa=True
+computador_ligado=False
+print("A luz esta acesa?",luz_acesa)
+print("O computador está ligado?",computador_ligado)
